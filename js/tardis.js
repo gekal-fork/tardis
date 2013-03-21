078ce66a11656c5a692b6056dcb1fa2f451b49d6
Luciano Cossich Sales <lucoweb@gmail.com>
2013-03-20 23:56:43 -0300

    Issue #1942536 by 20th: Javascript now adheres to Drupal standards\nIssue #1942536 by 20th, Luco: Page title input by user mustn't be run through t().

--- js/tardis.js
+++ js/tardis.js
@@ -3,21 +3,23 @@
  * This javascript adds accordion-style functionality to years and months.
  */
 
-(function($){
-  $(function(){
-    // The variable "year" takes the argument YYYY from the URL
-    // and is passed by Drupal.
-    year = Drupal.settings.tardis.year;
-    $("#tardis-accordion .tardis-link-year>*>a")
-      .attr("href", "javascript:void(0);");
-    $("#tardis-accordion .tardis-year-" + year + ">.item-list")
-      .addClass("active");
-    $("#tardis-accordion *>.item-list:not(.active)").hide();
-    $("#tardis-accordion .tardis-link-year")
-    .click(function(){
-      $(this).siblings().children(".item-list").hide();
-      $(this).children(".item-list").addClass("active");
-      $(this).children(".item-list.active").show();
-    });
-  })
-})(jQuery);
+(function($, Drupal){
+  Drupal.behaviors.tardis = {
+    attach: function (context, settings) {
+      // The variable "year" takes the argument YYYY from the URL
+      // and is passed by Drupal.
+      year = settings.tardis.year;
+      $("#tardis-accordion .tardis-link-year>*>a", context)
+        .attr("href", "javascript:void(0);");
+      $("#tardis-accordion .tardis-year-" + year + ">.item-list", context)
+        .addClass("active");
+      $("#tardis-accordion *>.item-list:not(.active)", context).hide();
+      $("#tardis-accordion .tardis-link-year", context).click(function () {
+        var $this = $(this);
+        $this.siblings().children(".item-list").hide();
+        $this.children(".item-list").addClass("active");
+        $this.children(".item-list.active").show();
+      });
+    }
+  };
+})(jQuery, Drupal);
